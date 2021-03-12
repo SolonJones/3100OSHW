a227379a630ce93af743e5992e55e490467d3b96
Solon Jones <imisuzu@hotmail.com>
2021-03-12 10:09:40 -0500

    all done

--- asmt3/task1/exe.sh
+++ asmt3/task1/exe.sh
@@ -8,13 +8,18 @@ file_name="task1.c"
 # compile the file 
 gcc -pthread $file_name -lm
 
-if [[ $? == 0 ]]; then 
+if [[ $? == 0 ]]; 
+then 
+	date >> log_task1.txt
 	./a.out 100 >> log_task1.txt ;
 	./a.out 1000 >> log_task1.txt ;
 	./a.out 10000 >> log_task1.txt ;
 	./a.out 100000 >> log_task1.txt ;
+	printf "\n\n" >> log_task1.txt
+	echo "successfully complete test and generated log file"
+else
+	echo "something went wrong, try to manually complie and run -- see README.md for details."
 fi 
 
-echo "successfully complete test and generated log file"
 
 
